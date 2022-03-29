d8a56524b781d3f0b3cad2826d1cc33796521dcb
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2022-03-29 23:06:22 +0300

    Included, tested and configured ExpressJS

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_PORT, DB_NAME, DB_USR, DB_PSW } = process.env;
+const { PORT, DB_HOST, DB_PORT, DB_NAME, DB_USR, DB_PSW } = process.env;
 console.log({ DB_HOST, DB_PORT, DB_NAME, DB_USR, DB_PSW });
 
 const mariadb = require('mariadb');
@@ -14,21 +14,35 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
-async function asyncRead() {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query("select * from hello_world");
-        rows.forEach(row => {
-            console.log(row);
-        });
-        await conn.end();
-        process.exit(0);
-    } catch (err) {
-        throw err;
-    } finally {
-        if (conn) return conn.end();
-    }
-}
+// async function asyncRead() {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const rows = await conn.query("select * from hello_world");
+//         rows.forEach(row => {
+//             console.log(row);
+//         });
+//         await conn.end();
+//         process.exit(0);
+//     } catch (err) {
+//         throw err;
+//     } finally {
+//         if (conn) return conn.end();
+//     }
+// }
 
-asyncRead().catch(console.error);
+// asyncRead().catch(console.error);
+
+const express = require('express');
+var app = express();
+app.listen(
+    PORT,
+    () => console.log(`LIVE: http://localhost:${PORT}/product`)
+);
+
+app.get('/product', (req, res) => {
+    const { id } = req.params;
+    res.status(200).send({
+        name: `${id}`
+    });
+});
