fc67c31d4f2b547d020691854a395585a13d6c58
Upsice <92324211+Upsice@users.noreply.github.com>
2022-04-27 10:30:48 +0300

    disabled httpcache

--- src/crawler/pbl/settings.py
+++ src/crawler/pbl/settings.py
@@ -93,7 +93,7 @@ ROBOTSTXT_OBEY = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+#HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
