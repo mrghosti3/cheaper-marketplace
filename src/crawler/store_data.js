f1beef7bd58a1cf3fe3056f55e2f1e411dee35f1
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2022-05-12 04:52:15 +0300

    implemented store_data.js

--- src/crawler/store_data.js
+++ src/crawler/store_data.js
@@ -10,9 +10,18 @@ const dataList = [
     // 'output_Moto.json',
     // 'output_Pigu.json',
     // 'output_Rimi.json',
-    // 'output_Topo.json'
+    'output_Topo.json'
 ];
 
+const conn = await createConnection({
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PSSW
+});
+
+await conn.beginTransaction();
 let lastID = 1;
 for (const file of dataList) {
     const relFile = dataDir + file;
@@ -21,11 +30,12 @@ for (const file of dataList) {
     entries.pdata = []
     for (const i in entries.products) {
         const p = entries.products[i];
+        const price = typeof p.Price == 'number' ? p.Price : parseFloat(p.Price.replaceAll('€', ''));
         const pid = lastID++;
         entries.pdata.push({
             pid: pid,
             sid: entries.shop.sid,
-            price: p.Price,
+            price: price, //.replace(',', '.')
             product_path: p.Link
         });
         entries.products[i] = {
@@ -35,4 +45,29 @@ for (const file of dataList) {
         };
     }
 
+    try {
+        await conn.query({
+            namedPlaceholders: true,
+            sql: 'INSERT INTO cheaper.shop(sid, name, domain, image_url) VALUES (:sid, :name, :domain, :image_url)'
+        },entries.shop);
+
+        await conn.batch({
+            namedPlaceholders: true,
+            sql: 'INSERT INTO cheaper.product(pid, name, image_url) VALUES (:pid, :name, :image_url)'
+        }, entries.products);
+
+        await conn.batch({
+            namedPlaceholders: true,
+            sql: 'INSERT INTO cheaper.pdata(pid, sid, price, product_path) VALUES (:pid, :sid, :price, :product_path)'
+        }, entries.pdata);
+
+    } catch (err) {
+        console.error(err);
+        process.exit(-2);
+    }
 }
+await conn.commit();
+
+await conn.end();
+console.log("End");
+process.exit(0);
