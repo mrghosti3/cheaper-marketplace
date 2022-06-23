95505dc8fbef2c785ee646995db9e8938cec7b80
shmitzas <46751666+shmitzas@users.noreply.github.com>
2022-06-23 16:37:46 +0300

    refactored database structure

--- src/database/setup_database.sql
+++ src/database/setup_database.sql
@@ -2,12 +2,6 @@
 DROP DATABASE IF EXISTS cheaper;
 CREATE DATABASE cheaper;
 
-CREATE TABLE cheaper.product (
-    pid        INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(50) NOT NULL,
-    image_url  VARCHAR(1024) NOT NULL DEFAULT 'http://www.domain.lt/product_image_path'
-);
-
 CREATE TABLE cheaper.shop (
     sid        INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name       VARCHAR(50)   NOT NULL UNIQUE,
@@ -15,44 +9,19 @@ CREATE TABLE cheaper.shop (
     image_url  VARCHAR(1024) NOT NULL DEFAULT 'http://www.domain.lt/shop_image_path'
 );
 
-CREATE TABLE cheaper.tag (
-    tid  INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    term VARCHAR(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE cheaper.pdata (
-    pid          INT UNSIGNED,
-    sid          INT UNSIGNED,
-    last_scan    DATE NOT NULL DEFAULT CURRENT_DATE,
-    price        DECIMAL(6, 2) UNSIGNED NOT NULL DEFAULT 1,
-    product_path VARCHAR(1024) NOT NULL DEFAULT '/product_path',
-    PRIMARY KEY(pid, sid, last_scan),
-    FOREIGN KEY(pid) REFERENCES cheaper.product(pid),
+CREATE TABLE cheaper.product (
+    pid             INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    sid             INT UNSIGNED,
+    name            VARCHAR(50) NOT NULL,
+    prod_url        VARCHAR(1024) NOT NULL DEFAULT 'http://www.domain.lt/product_path',
+    price           DECIMAL(6, 2) UNSIGNED NOT NULL DEFAULT 1,
+    image_url       VARCHAR(1024) NOT NULL DEFAULT 'http://www.domain.lt/product_image_path',
     FOREIGN KEY(sid) REFERENCES cheaper.shop(sid)
 );
 
-CREATE TABLE cheaper.product_tags (
-    tid INT UNSIGNED,
-    pid INT UNSIGNED,
-    PRIMARY KEY(tid, pid),
-    FOREIGN KEY(tid) REFERENCES cheaper.tag(tid),
+CREATE TABLE cheaper.scans (
+    pid         INT UNSIGNED,
+    last_scan   DATE NOT NULL DEFAULT CURRENT_DATE,
+    price       DECIMAL(6, 2) UNSIGNED NOT NULL DEFAULT 1,
     FOREIGN KEY(pid) REFERENCES cheaper.product(pid)
 );
-
-CREATE TABLE cheaper.shop_tags (
-    tid INT UNSIGNED,
-    sid INT UNSIGNED,
-    PRIMARY KEY(tid, sid),
-    FOREIGN KEY(tid) REFERENCES cheaper.tag(tid),
-    FOREIGN KEY(sid) REFERENCES cheaper.shop(sid)
-);
-
-CREATE VIEW cheaper.product_prices AS
-SELECT pid, sid, name, domain, s.image_url AS shop_image_url,
-       pd.last_scan AS last_scan, pd.price AS price,
-       concat(s.domain, pd.product_path) AS product_url
-FROM cheaper.pdata AS pd NATURAL JOIN cheaper.shop AS s;
-
-CREATE VIEW cheaper.combined_tags AS SELECT tid, pid, sid FROM cheaper.tag
-LEFT JOIN cheaper.shop_tags USING (tid)
-LEFT JOIN cheaper.product_tags USING (tid);
